8953b3cd4da815ad993f4c2c562018e080ac60e7
Johanna Keïta <veille.technologique.jk@gmail.com>
2025-02-24 20:25:46 +0100

    Added CreateCompanyPage and SubscribePage with components

--- TheOfficeGame-interface/src/pages/CreateCompanyPage.tsx
+++ TheOfficeGame-interface/src/pages/CreateCompanyPage.tsx
@@ -1,9 +1,18 @@
 import {FC} from 'react';
+import Header from "../components/Header.tsx";
+import Footer from "../components/Footer.tsx";
+
+
+import "../styles/CreateCompanyPage.css"
 
 const CreateCompanyPage: FC<{}> = ({}) => {
     return (
         <>
-            
+            <Header/>
+            <section className={"create-company-page-section"}>
+
+            </section>
+            <Footer/>
         </>
     );
 };
--- TheOfficeGame-interface/src/pages/SubscribePage.tsx
+++ TheOfficeGame-interface/src/pages/SubscribePage.tsx
@@ -2,6 +2,7 @@ import {FC} from 'react';
 import "../styles/SubscribePage.css"
 import Header from "../components/Header.tsx";
 import Footer from "../components/Footer.tsx";
+import SubscribeForm from "../components/forms/SubscribeForm.tsx";
 
 
 const SubscribePage: FC<{}> = ({}) => {
@@ -9,7 +10,7 @@ const SubscribePage: FC<{}> = ({}) => {
         <>
             <Header/>
             <section className={"subscribe-page-section"}>
-
+            <SubscribeForm/>
             </section>
             <Footer/>
         </>
