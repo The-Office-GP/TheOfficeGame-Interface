ca6641b7a2db122ff88ffb90de1976f33ec470f7
Johanna Keïta <veille.technologique.jk@gmail.com>
2025-02-24 20:19:02 +0100

    Added BuildingCompanyPage and components

--- TheOfficeGame-interface/src/pages/BuildingCompanyPage.tsx
+++ TheOfficeGame-interface/src/pages/BuildingCompanyPage.tsx
@@ -1,9 +1,19 @@
 import {FC} from 'react';
+import Header from "../components/Header.tsx";
+import BuildCompagnyForm from "../components/forms/BuildCompanyForm.tsx";
+import Footer from "../components/Footer.tsx";
+
+import "../styles/BuildingCompanyPage.css"
+
 
 const BuildingCompanyPage: FC<{}> = ({}) => {
     return (
         <>
-
+            <Header/>
+            <section className={"building-company-page-section"}>
+                <BuildCompagnyForm/>
+            </section>
+            <Footer/>
         </>
     );
 };
